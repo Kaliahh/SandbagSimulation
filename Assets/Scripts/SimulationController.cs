5e75c64de22a3d89f727e72780926c9dbb011d8a
Sivania <35276995+Sivania@users.noreply.github.com>
2019-04-08 12:52:12 +0200

    Add files via upload
    Flere funktioner:
    Bruger kan nu indtaste placering af diget for simulationen
    Forskudt sandsække genering (Så de ikke generer ovenpå hinaden og kollisionen skyder dem væk)

--- Assets/Scripts/SimulationController.cs
+++ Assets/Scripts/SimulationController.cs
@@ -8,41 +8,73 @@ namespace SandbagSimulation
     {
         // GameObject referencer
         public GameObject Drone;
+        public GameObject SandBag;
         private List<GameObject> Drones;
 
         // Variable brugeren kan ændre på
         public int DroneSpeed;
         public int NumberOfDrones;
+        public int NumberOfSandbags;
+
+        public Vector3 SandbagSpawnPoint; //Sandsæk genereringspunkt
+        public Vector3 DroneSpawnPoint; //Drone genereringspunkt
+
+        // Variable for diget (Lige nu kun 2 nodes)
+        public Vector3 Node1; //Starten af diget
+        public Vector3 Node2; //Enden af diget
+        public int Height;
 
         void Start()
         {
+            // Object Generering
             InitializeDrones();
             SetDroneSpeed();
-
-            // Blueprint eksempel
-            List<Vector3> nodes = new List<Vector3> { new Vector3(10f, 0f, 0f), new Vector3(-10f, 0f, 0f) };
-            Blueprint blueprint = new Blueprint(nodes, 10);
+            InitializeBlueprints();
+            InitializeSandBags();
         }
-        private void InitializeDrones()
+        private void InitializeDrones() //Genererer droner
         {
             Drones = new List<GameObject>();
             for (int i = 0; i < NumberOfDrones; i++)
             {
-                Drones.Add(Instantiate(Drone));
+                Drones.Add(Instantiate(Drone, DroneSpawnPoint, Quaternion.identity));
             }
         }
-        private void InitializeBlueprints()
+        private void InitializeBlueprints() //Giver dronerne blueprintet
+        {
+            // Blueprint generering
+            List<Vector3> nodes = new List<Vector3> { Node1, Node2 };
+            Blueprint blueprint = new Blueprint(nodes, 10);
+
+            foreach (GameObject drone in Drones) //Giver en kopi til hver drone
+            {
+                //Kan ikke få adgang til felterne for objekter
+
+
+                //drone.GetComponent<DroneController>().MyBlueprint = blueprint;
+            }
+        }
+        private void SetDroneSpeed() //Sætter dronernes fart
         {
             foreach (GameObject drone in Drones)
             {
-                //NEED TO IMPLEMENT BLUEPRINT OF DRONECONTROLLER
+                //Kan ikke få adgang til felterne for objekter
+
+                //DroneMovement droneMovement = new DroneMovement();
+                //drone.AddComponent(DroneMovement(typeof(script)));
+                //drone.GetComponent<DroneController>().ViewDistance = 15f;
+                //drone.GetComponent<DroneController>().MyMovement = droneMovement;
+                //
+                //DroneController.MyMovement.Speed = DroneSpeed;
             }
         }
-        private void SetDroneSpeed()
+        private void InitializeSandBags() //Generer sandsække
         {
-            foreach (GameObject drone in Drones)
+            Vector3 NewSandbagSpawnPoint; //Varible for a new spawnposition
+            for (int i = 0; i < NumberOfSandbags; i++)
             {
-                //NEED SET THE SPEED OF DRONECONTROLLER 
+                Vector3 NewPos = SandbagSpawnPoint + new Vector3(2f * i, 0f, 0f); //Logik for genereringspunkt for enkelte sandsække
+                Instantiate(SandBag, NewPos, Quaternion.identity);
             }
         }
     }
