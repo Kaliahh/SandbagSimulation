1f9ae8f43a83d65624fdd5d880792f2e74dcbe97
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-04-09 12:27:48 +0200

    SimulationController: Implementeret nogle dele, SetDroneSpeed virker ikke

--- Assets/Scripts/SimulationController.cs
+++ Assets/Scripts/SimulationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,9 +13,10 @@ namespace SandbagSimulation
         private List<GameObject> Drones;
 
         // Variable brugeren kan ændre på
-        public int DroneSpeed;
+        public float DroneSpeed;
         public int NumberOfDrones;
         public int NumberOfSandbags;
+        // TODO: Tilføj DroneViewDistance og en method der kan sætte den hos dronerne
 
         public Vector3 SandbagSpawnPoint; //Sandsæk genereringspunkt
         public Vector3 DroneSpawnPoint; //Drone genereringspunkt
@@ -30,53 +32,73 @@ namespace SandbagSimulation
             InitializeDrones();
             SetDroneSpeed();
             InitializeBlueprints();
-            InitializeSandBags();
+
+            this.GetComponent<SandbagSpawner>().SpawnPoint = SandbagSpawnPoint;
+            SetDroneSandbagPickUpLocation();
+
+            //InitializeSandBags();
         }
-        private void InitializeDrones() //Genererer droner
+
+        private void SetDroneSandbagPickUpLocation()
         {
+            Vector3 location = new Vector3(SandbagSpawnPoint.x, SandbagSpawnPoint.y + 5, SandbagSpawnPoint.z);
+            foreach (GameObject drone in Drones)
+            {
+                drone.GetComponent<DroneController>().SetSandbagPickUpLocation(location);
+            }
+        }
+
+        // Instansierer droner
+        private void InitializeDrones() 
+        {
+            float j = 0;
             Drones = new List<GameObject>();
+
             for (int i = 0; i < NumberOfDrones; i++)
             {
-                Drones.Add(Instantiate(Drone, DroneSpawnPoint, Quaternion.identity));
+                if (i % 3 == 0)
+                {
+                    j++;
+                }
+                Vector3 NewDroneSpawnPoint = new Vector3(DroneSpawnPoint.x + j * 3, DroneSpawnPoint.y, DroneSpawnPoint.z + (i % 3) * 3);
+
+                Drones.Add(Instantiate(Drone, NewDroneSpawnPoint, Quaternion.identity));
             }
         }
-        private void InitializeBlueprints() //Giver dronerne blueprintet
+
+        // Genererer blueprints og giver dem til dronerne
+        private void InitializeBlueprints() 
         {
             // Blueprint generering
-            List<Vector3> nodes = new List<Vector3> { Node1, Node2 };
-            Blueprint blueprint = new Blueprint(nodes, 10);
+            List<Vector3> ConstructionNodes = new List<Vector3> { Node1, Node2 };
+            Blueprint blueprint = new Blueprint(ConstructionNodes, 10);
 
-            foreach (GameObject drone in Drones) //Giver en kopi til hver drone
+            // Giver en kopi af blueprintet til hver drone
+            foreach (GameObject drone in Drones) 
             {
-                //Kan ikke få adgang til felterne for objekter
-
-
-                //drone.GetComponent<DroneController>().MyBlueprint = blueprint;
+                drone.GetComponent<DroneController>().SetBlueprint(blueprint);
             }
         }
-        private void SetDroneSpeed() //Sætter dronernes fart
+
+        // Sætter dronernes fart
+        private void SetDroneSpeed() 
         {
             foreach (GameObject drone in Drones)
             {
-                //Kan ikke få adgang til felterne for objekter
+                // TODO: Virker ikke
+                // drone.GetComponent<DroneController>().SetSpeed(DroneSpeed);
+            }
+        }
 
-                //DroneMovement droneMovement = new DroneMovement();
-                //drone.AddComponent(DroneMovement(typeof(script)));
-                //drone.GetComponent<DroneController>().ViewDistance = 15f;
-                //drone.GetComponent<DroneController>().MyMovement = droneMovement;
-                //
-                //DroneController.MyMovement.Speed = DroneSpeed;
-            }
-        }
-        private void InitializeSandBags() //Generer sandsække
-        {
-            Vector3 NewSandbagSpawnPoint; //Varible for a new spawnposition
-            for (int i = 0; i < NumberOfSandbags; i++)
-            {
-                Vector3 NewPos = SandbagSpawnPoint + new Vector3(2f * i, 0f, 0f); //Logik for genereringspunkt for enkelte sandsække
-                Instantiate(SandBag, NewPos, Quaternion.identity);
-            }
-        }
+        // Generer sandsække
+        //private void InitializeSandBags() 
+        //{
+        //    for (int i = 0; i < NumberOfSandbags; i++)
+        //    {
+        //        Vector3 NewPos = SandbagSpawnPoint + new Vector3(2f * i, 0f, 0f); //Logik for genereringspunkt for enkelte sandsække
+        //        Instantiate(SandBag, NewPos, Quaternion.identity);
+        //    }
+        //}
     }
 }
 
