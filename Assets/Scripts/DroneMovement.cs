baba1c4fd050357b6da026ec9faa0bda2b20d7c9
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-04-09 12:26:24 +0200

    Sandsækken følger nu med dronen

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DroneMovement : MonoBehaviour
+
+namespace SandbagSimulation
 {
-    public float Speed;
-    public float Step { get; private set; }
-
-    private void Awake()
+    public class DroneMovement : MonoBehaviour
     {
-        Step = Speed * Time.deltaTime;
-    }
+        public float Speed;
+        private float Step;
 
-    // Flytter GameObject et Step mod Vector3 target i løbet af en enkelt frame.
-    public void FlyTo(Vector3 target)
-    {
-        this.transform.position = Vector3.MoveTowards(this.transform.position, target, Step);
+        // Flytter GameObject et Step mod Vector3 target i løbet af en enkelt frame.
+        public void FlyTo(Vector3 target)
+        {
+            Step = Speed * Time.deltaTime;
+
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target, Step);
+
+            if (this.GetComponent<DroneController>().MySandbag != null)
+            {
+                Vector3 sandbagTarget = new Vector3(this.transform.position.x, this.transform.position.y - 1f, this.transform.position.z);
+
+                this.GetComponent<DroneController>().MySandbag.transform.position = sandbagTarget;
+                this.GetComponent<DroneController>().MySandbag.transform.rotation = Quaternion.identity;
+
+                //this.GetComponent<DroneController>().MySandbag.transform.position =
+                //    Vector3.MoveTowards(this.GetComponent<DroneController>().MySandbag.transform.position, sandbagTarget, Step);
+            }
+        }
     }
 }
