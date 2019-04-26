e0088b5989dfc3060a6c03eecb4d2b9ae0fd1274
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-04-26 16:03:12 +0200

    Ændret DroneMovement til DroneMover

--- Assets/Scripts/DroneMover.cs
+++ Assets/Scripts/DroneMover.cs
@@ -1,19 +1,33 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 
 namespace SandbagSimulation
 {
-    public class DroneMovement : MonoBehaviour
+    public class DroneMover : MonoBehaviour
     {
         public float Speed;
         private float Step;
+
+        public List<GameObject> OtherDrones;
 
         // Flytter GameObject et Step mod Vector3 target i løbet af en enkelt frame.
         public void FlyTo(Vector3 target)
         {
             Step = Speed * Time.deltaTime;
+
+            //if (IsPathBlocked(target) == true)
+            //{
+            //    // Flyv udenom eller bliv
+            //}
+
+            //else
+            //{
+            //    // this.transform.position = Vector3.MoveTowards(this.transform.position, target, Step);
+            //}
 
             this.transform.position = Vector3.MoveTowards(this.transform.position, target, Step);
 
@@ -24,6 +38,16 @@ namespace SandbagSimulation
                 this.GetComponent<DroneController>().MySandbag.transform.position = sandbagTarget;
                 this.GetComponent<DroneController>().MySandbag.transform.rotation = Quaternion.identity;
             }
+        }
+
+        private bool IsPathBlocked(Vector3 target)
+        {
+            int blockingDrones = OtherDrones
+                .Where(p => (this.transform.position - p.transform.position).magnitude < 4)
+                .Where(p => (Vector3.Project((this.transform.position - p.transform.position), target) - p.transform.position).magnitude < 4)
+                .Count();
+
+            return blockingDrones > 0;
         }
     }
 }
