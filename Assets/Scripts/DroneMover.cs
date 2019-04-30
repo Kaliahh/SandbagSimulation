6e1eaa20ef052d6435cb9c045e0654dff91a70e3
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-04-30 12:54:14 +0200

    Dronen flyver nu på en lidt anden måde, ikke med MoveTowards

--- Assets/Scripts/DroneMover.cs
+++ Assets/Scripts/DroneMover.cs
@@ -27,26 +27,71 @@ namespace SandbagSimulation
             //    // this.transform.position = Vector3.MoveTowards(this.transform.position, target, Step);
             //}
 
-            this.transform.position = Vector3.MoveTowards(this.transform.position, target, Speed * Time.deltaTime);
+            Vector3 direction = target - this.transform.position;
+
+            //direction += AvoidCollision(direction);
+
+            Debug.DrawLine(this.transform.position, this.transform.position + direction.normalized);
+
+            //this.transform.position = Vector3.MoveTowards(this.transform.position, direction.normalized, Speed * Time.deltaTime);
+
+            this.transform.position += ((direction.magnitude > 1) ? direction.normalized : direction) * Speed * Time.deltaTime;
+
+            //this.transform.position = Vector3.MoveTowards(this.transform.position, (direction.magnitude > 1) ? direction.normalized : direction, Speed * Time.deltaTime);
 
             // Hvis der er blevet samlet en sandsæk op, følger den med dronen
             if (this.GetComponent<DroneController>().MySandbag != null)
             {
-                Vector3 sandbagTarget = new Vector3(this.transform.position.x, this.transform.position.y - 1f, this.transform.position.z);
-
-                this.GetComponent<DroneController>().MySandbag.transform.position = sandbagTarget;
-                this.GetComponent<DroneController>().MySandbag.transform.rotation = Quaternion.identity;
+                MoveSandbag();
             }
         }
 
-        private bool IsPathBlocked(Vector3 target)
+        private Vector3 AvoidCollision(Vector3 destination)
         {
-            int blockingDrones = OtherDrones
-                .Where(p => (this.transform.position - p.transform.position).magnitude < 4)
-                .Where(p => (Vector3.Project((this.transform.position - p.transform.position), target) - p.transform.position).magnitude < 4)
-                .Count();
+            Vector3 avoidanceVector = Vector3.zero;
+            Vector3 direction = Vector3.zero;
 
-            return blockingDrones > 0;
+            float distance = 0;
+
+            var a = OtherDrones
+                .Where(d => Vector3.Distance(d.transform.position, this.transform.position) < 2)
+                .ToList();
+
+            foreach (GameObject drone in a)
+            {
+                direction = this.transform.position - drone.transform.position;
+                distance = direction.magnitude;
+
+                avoidanceVector += ((2 / distance) * direction);
+            }
+
+            //Vector3 resultant = Vector3.zero;
+
+            //foreach (GameObject drone in OtherDrones)
+            //{
+            //    resultant += (this.transform.position - drone.transform.position) * 1 / (Vector3.Distance(this.transform.position, drone.transform.position));
+            //}
+
+            return avoidanceVector;
         }
+
+        private void MoveSandbag()
+        {
+            Vector3 sandbagTarget = new Vector3(this.transform.position.x, this.transform.position.y - 1f, this.transform.position.z);
+
+            this.GetComponent<DroneController>().MySandbag.transform.position = sandbagTarget;
+            this.GetComponent<DroneController>().MySandbag.transform.rotation = Quaternion.identity;
+        }
+
+
+        //private bool IsPathBlocked(Vector3 target)
+        //{
+        //    int blockingDrones = OtherDrones
+        //        .Where(p => (this.transform.position - p.transform.position).magnitude < 4)
+        //        .Where(p => (Vector3.Project((this.transform.position - p.transform.position), target) - p.transform.position).magnitude < 4)
+        //        .Count();
+
+        //    return blockingDrones > 0;
+        //}
     }
 }
