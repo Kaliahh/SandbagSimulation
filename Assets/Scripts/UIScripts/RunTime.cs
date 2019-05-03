ccdf4667bab313b6cf24ca990e1738c06a76a0ee
Sivania <35276995+Sivania@users.noreply.github.com>
2019-05-03 13:13:14 +0200

    Add files via upload
    UI som er afkoblet fra projektet. Skal implementeres senere.

---
+++ Assets/Scripts/UIScripts/RunTime.cs
@@ -0,0 +1,61 @@
+﻿using SandbagSimulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunTime : MonoBehaviour
+{
+    private Text Time; //Tid
+    private Text SandbagsLeft; //Amount of sandbags left before simulation is ended
+
+    private GameObject Menu; //Menu under simulation kørsel
+    void Start() 
+    {
+        //Referencer
+        Time = GameObject.Find("Time").GetComponent<Text>(); //Tid
+        SandbagsLeft = GameObject.Find("SandBags").GetComponent<Text>(); //Antallet af sandsække
+        Menu = GameObject.Find("Menu");
+        Menu.SetActive(false);
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            ToggleMenu();
+        }
+    }
+
+    private void ToggleMenu()
+    {
+        if (Menu.active) //If Menuen allerede er aktiv 
+        {
+            Menu.SetActive(false); //Fjern menuen
+        }
+        else //Ellers 
+        {
+            Menu.SetActive(true); //Åben menuen
+        }
+    }
+    
+    public void ChangeTime(float seconds) 
+    {
+        //Time.text = seconds.ToString();
+    }
+
+    public void ChangeSandBagsLeft(float sandbags)
+    {
+        //SandBagsLeft.text = seconds.ToString();
+    }
+
+    //Menu Methods
+
+    public void ChangeSpeed(float speed) 
+    {
+        //GameObject a = Menu.Find("Slider");
+    }
+
+
+
+}
---
+++ Assets/Scripts/UIScripts/UI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SandbagSimulation
+{
+    public class UI : MonoBehaviour
+    {
+        //UI sider
+        private GameObject MainMenu; //Startmenu UI
+        private GameObject RunTime; //UI under simulationen
+        private GameObject Results; //Resultatvindue når simulationen sluttes
+
+        private List<GameObject> UIs; //Liste med alle sider. Nemmere at kontrollere siderne via en liste
+
+        void Start()
+        {
+            MainMenu = transform.Find("MainMenu").gameObject; //Reference til StartMenu UI
+            RunTime = transform.Find("RunTime").gameObject; //Reference til runtime UI
+            Results = transform.Find("Results").gameObject; //Reference til resultatvinduet
+
+            UIs = new List<GameObject> { MainMenu, RunTime, Results }; //Siderne lægges ind i listen
+
+            StartMainMenu(); //Vis start menuen når programmet startes
+        }
+
+        public void StartMainMenu() //Show main menu. Is called when program starts or user returns to main menu
+        {
+            foreach (GameObject ui in UIs)
+            {
+                if (ui.name == "MainMenu") //Kun vis start menuen
+                    ui.SetActive(true);
+                else
+                    ui.SetActive(false);
+            }
+        }
+
+        public void StartRunTime() //Begin simulation
+        {
+            foreach (GameObject ui in UIs)
+            {
+                if (ui.name == "RunTime") //Kun vis start menuen
+                    ui.SetActive(true);
+                else
+                    ui.SetActive(false);
+            }
+        }
+
+        public void ShowResults() //End simulation. Is called when all sandbags are placed
+        {
+            Results.SetActive(true);
+        }
+        public void HideResults()
+        {
+            Debug.Log("Tes");
+            Results.SetActive(false);
+        }
+    }
+}
+
