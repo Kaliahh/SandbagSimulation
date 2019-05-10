fb485bfc4c28a599127951cc379ea219a057ce9b
Unknown <jakob.soenderby@hotmail.com>
2019-05-10 13:33:28 +0200

    Ryddet op i FindPlace PlaymodeTests

--- Assets/PlaymodeTests/SectionTests/FindPlacePlaymodeTests.cs
+++ Assets/PlaymodeTests/SectionTests/FindPlacePlaymodeTests.cs
@@ -15,34 +15,11 @@ namespace Tests
     public class FindPlacePlaymodeTests
     {
         // Tester om der kommer et gyldigt output, givent et gyldigt input
-        /*
-        [Test]
-        public void FindPlace_PassValid_ReturnValid()
-        {
-            Vector3 position = new Vector3(0f, 10f, 10f);
-            Section section = new Section();
-            float viewDistance = 50f;
-            List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
-            constructionNodes.Add(new Vector3(0f, 0f, 0f));
-            // Right
-            constructionNodes.Add(new Vector3(0f, 0f, 20f));
-            Blueprint blueprint = new Blueprint(constructionNodes, 10);
-            // Lav sandsæk lige under dronen
-            GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube1.tag = "PlacedSandbag";
-            cube1.transform.position = new Vector3(0f, 0f, 10f);
-            cube1.AddComponent(typeof(SandbagController));
-
-            Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
-            //Assert.AreEqual(result, new Vector3(15f, 10f, 0f));
-            Assert.AreEqual(0, result.Length);
-        }
-        */
 
         [UnityTest]
         public IEnumerator FindPlace_PassValid_ReturnValid()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 5f, 10f);
             Section section = new Section();
             float viewDistance = 10f;
@@ -56,50 +33,43 @@ namespace Tests
             cube1.transform.position = new Vector3(0f, 0f, 10f);
             cube1.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
-
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
-            //Assert.AreEqual(result, new Vector3(15f, 10f, 0f));
+
+            //Assert
             Assert.IsNotNull(result);
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
         public IEnumerator FindPlace_PassSingleSandbag_ReturnTwoPlaces()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 10f);
             Section section = new Section();
             float viewDistance = 10f;
             List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
             constructionNodes.Add(new Vector3(0f, 0f, 0f));
-            // Right
             constructionNodes.Add(new Vector3(0f, 0f, 20f));
             Blueprint blueprint = new Blueprint(constructionNodes, 10);
 
-            // Lav sandsæk lige under dronen
-            /*
-            GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube1.tag = "PlacedSandbag";
-            cube1.transform.position = new Vector3(0f, 0f, 10f);
-            cube1.AddComponent(typeof(SphereCollider));
-            cube1.AddComponent(typeof(SandbagController));
-            */
             GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube1.tag = "PlacedSandbag";
             cube1.transform.position = new Vector3(0f, 0f, 10f);
             cube1.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
-
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
+
+            //Assert
             Assert.AreEqual(2, result.Length);
         }
 
         [UnityTest]
         public IEnumerator FindPlace_NoSandbagHit_ReturnNull()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 10f);
             Section section = new Section();
             float viewDistance = 10f;
@@ -108,26 +78,26 @@ namespace Tests
             constructionNodes.Add(new Vector3(0f, 0f, 20f));
             Blueprint blueprint = new Blueprint(constructionNodes, 5);
 
+            //Act
             yield return new WaitForFixedUpdate();
-
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
+
+            //Assert
             Assert.IsNull(result);
         }
 
         [UnityTest]
         public IEnumerator FindPlace_PassOneBagInViewOneOutOfView_ReturnTwoPlaces()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 10f);
             Section section = new Section();
             float viewDistance = 10f;
             List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
             constructionNodes.Add(new Vector3(0f, 0f, 0f));
-            // Right
             constructionNodes.Add(new Vector3(0f, 0f, 50f));
             Blueprint blueprint = new Blueprint(constructionNodes, 10);
 
-            // Lav sandsæk lige under dronen
             GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube1.tag = "PlacedSandbag";
             cube1.transform.position = new Vector3(0f, 0f, 10f);
@@ -138,36 +108,36 @@ namespace Tests
             cube2.transform.position = new Vector3(0f, 0f, 40f);
             cube2.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
-
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
+
+            //Assert
             Assert.AreEqual(2, result.Length);
         }
 
         [UnityTest]
         public IEnumerator FindPlace_PassTwoBagsInView_ReturnThreePlaces()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 10f);
             Section section = new Section();
             float viewDistance = 20f;
             List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
             constructionNodes.Add(new Vector3(0f, 0f, 0f));
-            // Right
             constructionNodes.Add(new Vector3(0f, 0f, 50f));
             Blueprint blueprint = new Blueprint(constructionNodes, 10);
 
-            // Lav sandsæk lige under dronen
             GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube1.tag = "PlacedSandbag";
             cube1.transform.position = new Vector3(0f, 0f, 10f);
             cube1.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
 
             float length = cube1.GetComponent<SandbagController>().Length;
 
-            // Lav sandsæk uden for viewdistance
             GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube2.tag = "PlacedSandbag";
             cube2.transform.position = new Vector3(0f, 0f, 10f + length);
@@ -178,61 +148,58 @@ namespace Tests
             yield return new WaitForFixedUpdate();
 
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
-            foreach (Vector3 res in result)
-                Debug.Log(res.ToString());
+
+            //Assert
             Assert.AreEqual(3, result.Length);
         }
 
         [UnityTest]
         public IEnumerator FindPlace_PassStartingSandbagNotDirectlyUnder_ReturnTwoPlaces()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 10f);
             Section section = new Section();
             float viewDistance = 10f;
             List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
             constructionNodes.Add(new Vector3(0f, 0f, 0f));
-            // Right
             constructionNodes.Add(new Vector3(0f, 0f, 20f));
             Blueprint blueprint = new Blueprint(constructionNodes, 10);
 
-            // Lav sandsæk lige under dronen
             GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube1.tag = "PlacedSandbag";
             cube1.transform.position = new Vector3(0f, 0f, 15f);
             cube1.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
-            foreach (Vector3 place in result)
-                Debug.Log(place.ToString());
+
+            //Assert
             Assert.AreEqual(2, result.Length);
         }
         
         [UnityTest]
         public IEnumerator FindPlace_PassCompleteDike_ReturnTwoPositions()
         {
+            //Arrange
             Vector3 position = new Vector3(0f, 3f, 11f);
             Section section = new Section();
             float viewDistance = 20f;
             List<Vector3> constructionNodes = new List<Vector3>();
-            // Left
             constructionNodes.Add(new Vector3(0f, 0f, 10.5f));
-            // Right
             constructionNodes.Add(new Vector3(0f, 0f, 11.5f));
             Blueprint blueprint = new Blueprint(constructionNodes, 2);
 
-            // Lav sandsæk lige under dronen
             GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube1.tag = "PlacedSandbag";
             cube1.transform.position = new Vector3(0f, 0f, 11f);
             cube1.AddComponent(typeof(SandbagController));
 
+            //Act
             yield return new WaitForFixedUpdate();
-
             Vector3[] result = section.FindPlace(viewDistance, position, blueprint);
-            foreach (Vector3 res in result)
-                Debug.Log(res.ToString());
+            
+            //Assert
             Assert.AreEqual(2, result.Length);
         }
     }
